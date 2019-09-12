e5a3685a3ba67be8c8be1468c10f5cda25325e9d
Nathan Acosta <git@nathanacosta.com>
2019-09-12 09:08:28 -0600

    feat: show ProfileNotFound component if handle not found

--- app/src/components/Profile.jsx
+++ app/src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import {
   makeStyles,
   Typography,
@@ -29,6 +29,7 @@ const useStyles = makeStyles(theme => ({
     borderRadius: '50%',
     borderWidth: 10,
     borderStyle: 'solid',
+    backgroundColor: 'grey',
     '& img': {
       width: '100%',
       maxWidth: '100%',
@@ -50,22 +51,46 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-function Profile(props) {
+function ProfileNotFound({ unknownHandle }) {
+  const classes = useStyles();
+  return (
+    <div style={{ backgroundColor: '#555' }}>
+      <div className={classes.header}>
+        <div className={classes.headerBanner} />
+      </div>
+      <div className={classes.profileImage} />
+      <Grid container direction="column">
+        <Grid item>
+          <Typography variant="body1" component="span">{`@${unknownHandle}`}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography align="center" variant="body1">This account does not exist</Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+ProfileNotFound.propTypes = {
+  unknownHandle: PropTypes.string.isRequired,
+};
+
+function Profile({ match }) {
   const [editOpen, setEditOpen] = React.useState(false);
   const [profile, setProfile] = React.useState({});
+  const [handleNotFound, setHandleNotFound] = React.useState(false);
   const classes = useStyles();
 
   const { profile: currUserProfile } = React.useContext(profileContext);
   const { handle: currUserHandle } = currUserProfile;
 
-  const { handle: pathHandle } = props.match.params;
+  const { handle: pathHandle } = match.params;
   const isCurrentUser = currUserHandle === pathHandle;
 
   const profileHandle = isCurrentUser ? currUserHandle : pathHandle;
   React.useEffect(() => {
     fetchProfile(profileHandle)
       .then(setProfile)
-      .catch(() => setProfile({}));
+      .catch(() => setHandleNotFound(true));
   }, [profileHandle]);
 
   const {
@@ -78,10 +103,6 @@ function Profile(props) {
     joinDate,
   } = profile;
 
-  const outputHandle = `@${handle}`;
-  const outputFollowing = `${followingCount} Following`;
-  const outputFollowers = `${followerCount} Followers`;
-
   const dateString = new Date(joinDate).toLocaleDateString('en-US', {
     month: 'long',
     year: 'numeric',
@@ -93,61 +114,75 @@ function Profile(props) {
   let websiteDisplayText = website;
   if (website) {
     // remove protocol if present
-    if (website.startsWith('http://')) websiteDisplayText = website.slice(7);
-    else if (website.startsWith('https://')) websiteDisplayText = website.slice(8);
+    if (website.startsWith('http://')) {
+      websiteDisplayText = website.slice(7);
+    }
+    else if (website.startsWith('https://')) {
+      websiteDisplayText = website.slice(8);
+    }
+
     // remove www if present
-    if (websiteDisplayText.startsWith('www')) websiteDisplayText = websiteDisplayText.slice(4);
+    if (websiteDisplayText.startsWith('www')) {
+      websiteDisplayText = websiteDisplayText.slice(4);
+    }
+  }
+
+  let profileMenu;
+  if (handleNotFound) {
+    return <ProfileNotFound unknownHandle={profileHandle} />;
+  }
+
+  if (isCurrentUser) {
+    profileMenu = (
+      <Grid container justify="flex-end" spacing={1}>
+        <Button
+          name="editProfile"
+          variant="outlined"
+          color="primary"
+          onClick={() => setEditOpen(true)}
+        >
+        Edit profile
+        </Button>
+        <EditProfile open={editOpen} onClose={() => setEditOpen(false)} />
+      </Grid>
+    );
+  }
+  else {
+    profileMenu = (
+      <Grid container justify="flex-end" spacing={1}>
+        <Button
+          name=""
+          variant="outlined"
+          color="primary"
+        >
+        Share Link
+        </Button>
+        <Button
+          name=""
+          variant="outlined"
+          color="primary"
+        >
+        Follow
+        </Button>
+      </Grid>
+    );
   }
 
   return (
     <div style={{ backgroundColor: '#555' }}>
       <div className={classes.header}>
         <div className={classes.headerBanner} />
-        {
-          isCurrentUser === true
-            ? (
-              <Grid container justify="flex-end" spacing={1}>
-                <Button
-                  name="editProfile"
-                  variant="outlined"
-                  color="primary"
-                  onClick={() => setEditOpen(true)}
-                >
-                Edit profile
-                </Button>
-                <EditProfile open={editOpen} onClose={() => setEditOpen(false)} />
-              </Grid>
-            ) : (
-              <Grid container justify="flex-end" spacing={1}>
-                <Button
-                  name=""
-                  variant="outlined"
-                  color="primary"
-                >
-                Share Link
-                </Button>
-                <Button
-                  name=""
-                  variant="outlined"
-                  color="primary"
-                >
-                Follow
-                </Button>
-              </Grid>
-            )
-        }
-
-
+        { profileMenu }
       </div>
       <div className={classes.profileImage}>
-        <img src={profileImageSrc} alt={`${name} profile`} />
+        {profileImageSrc && <img src={profileImageSrc} alt={`${name} profile`} />}
       </div>
       <Grid container direction="column">
         <Grid item>
           <Typography component="h3" className={classes.profileName}>{name}</Typography>
         </Grid>
         <Grid item>
-          <Typography variant="body1" component="span">{outputHandle}</Typography>
+          <Typography variant="body1" component="span">{`@${handle}`}</Typography>
         </Grid>
         <Grid item>
           <Grid container alignItems="center">
@@ -162,30 +197,46 @@ function Profile(props) {
             {website && (
               <Grid item container alignItems="center">
                 <LinkIcon fontSize="small" />
-                <Typography variant="body1" className={classes.marginRight} component="a" href={website} target="_blank">
+                <Typography
+                  variant="body1"
+                  className={classes.marginRight}
+                  component="a"
+                  href={website}
+                  target="_blank"
+                >
                   {websiteDisplayText}
                 </Typography>
               </Grid>
             )}
-            {joinDate && (
-              <Grid item container alignItems="center">
-                <JoinDateIcon fontSize="small" />
-                <Typography variant="body1" component="span">
-                  {outputJoinedDate}
-                </Typography>
-              </Grid>
-            )}
+            <Grid item container alignItems="center">
+              <JoinDateIcon fontSize="small" />
+              <Typography variant="body1" component="span">
+                {outputJoinedDate}
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
         <Grid item>
           <Typography variant="body1" component="span" className={classes.marginRight}>
-            {outputFollowing}
+            {`${followingCount} Following`}
           </Typography>
-          <Typography variant="body1" component="span">{outputFollowers}</Typography>
+          <Typography variant="body1" component="span">
+            {`${followerCount} Followers`}
+          </Typography>
         </Grid>
       </Grid>
     </div>
   );
 }
+Profile.defaultProps = {
+  match: { params: { handle: '' } },
+};
+Profile.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      handle: PropTypes.string,
+    }),
+  }),
+};
 
 export default Profile;
